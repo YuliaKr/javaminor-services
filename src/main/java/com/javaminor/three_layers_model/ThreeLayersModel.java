19ea086d6d6c574bc2fee3db7c7c830e3252c82b
Yulia <0937630@hr.nl>
2019-10-07 20:47:02 +0200

    initial commit

---
+++ src/main/java/com/javaminor/three_layers_model/ThreeLayersModel.java
@@ -0,0 +1,13 @@
+package com.javaminor.three_layers_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeLayersModel {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeLayersModel.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaminor/three_layers_model/ThreeLayersModelTests.java
@@ -0,0 +1,15 @@
+package com.javaminor.three_layers_model;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThreeLayersModelTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
